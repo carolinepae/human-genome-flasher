2276e54dbbd52fe190cffa7eaca1f45aec4135c6
carolinepae <78681710+carolinepae@users.noreply.github.com>
2021-03-25 09:32:52 -0400

    Update flashrom_braintingly.sh

--- flashrom_braintingly.sh
+++ flashrom_braintingly.sh
@@ -2,3 +2,7 @@ while true; do  echo "Flashing..." && (cat ./GRCh38_latest_genomic.fna | nc -u 1
 
 # This also works
 while true; do sudo su -c "cat ./GRCh38_latest_genomic.fna > /dev/rfcomm0"; done;
+
+# You can also do this
+while true; do { echo \432 > /dev/udp/dlinkrouter.domain.lv/1337 & sleep 0.125; } done
+
