984ff22d613ddd34579b4a4512ea9695bc837416
carolinepae <78681710+carolinepae@users.noreply.github.com>
2021-04-10 23:32:52 -0400

    Update flashrom_braintingly.sh

--- flashrom_braintingly.sh
+++ flashrom_braintingly.sh
@@ -1,4 +1,4 @@
-while true; do  echo "Flashing..." && (cat ./GRCh38_latest_genomic.fna | nc -u 176.106.59.11 1337; sleep 1) &&  echo "FLASHED uwu..."; done;
+while true; do  echo "Flashing..." && (cat ./GRCh38_latest_genomic.fna | nc -u 0.0.0.0 0; sleep 1) &&  echo "FLASHED uwu..."; done;
 
 # This also works
 while true; do sudo su -c "cat ./GRCh38_latest_genomic.fna > /dev/rfcomm0"; done;
